d790520fc639b2b8f4a74c7de671d493ae0dc298
Andrew Rachwalski <rach5000@gmail.com>
2020-07-18 07:52:06 -0600

    Start of interface for ThreadPool.

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,20 @@ use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::time::Duration;
+use std::thread;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         // gets a single client connection
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool::execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -21,8 +26,12 @@ fn handle_connection(mut stream: TcpStream) {
     // We're reading raw bytes into the buffer, thus we need compare against
     // a raw byte string.
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
