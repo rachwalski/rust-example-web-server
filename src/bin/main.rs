62b48db3fadabef89cd4284f2c9cd28cd19c386b
Andrew Rachwalski <rach5000@gmail.com>
2020-07-21 21:51:12 -0600

    Buggy implementation of graceful shutdown.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -11,7 +11,8 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     let pool = ThreadPool::new(4);
 
-    for stream in listener.incoming() {
+    //for stream in listener.incoming() {
+    for stream in listener.incoming().take(2) {
         // gets a single client connection
         let stream = stream.unwrap();
 
